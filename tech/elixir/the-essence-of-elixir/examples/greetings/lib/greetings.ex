24dae996867b83c23043951fd918b290eaf381fa
Sascha Wolf <swolf.dev@gmail.com>
2019-12-11 21:47:09 +0100

    Examples: Add a `greetings` example project

---
+++ tech/elixir/the-essence-of-elixir/examples/greetings/lib/greetings.ex
@@ -0,0 +1,13 @@
+defmodule Greetings do
+  @doc """
+  Prints a friendly greeting.
+
+  ## Examples
+
+      iex> Greetings.hello("Web Engineering Düsseldorf")
+      "Hello Web Engineering Düsseldorf!"
+  """
+  def hello(name) do
+    "Hello #{name}"
+  end
+end
---
+++ tech/elixir/the-essence-of-elixir/examples/greetings/test/greetings_test.exs
@@ -0,0 +1,10 @@
+defmodule GreetingsTest do
+  use ExUnit.Case, async: true
+
+  doctest Greetings
+
+  # test "it greets friendly" do
+  #   assert Greetings.hello("Web Engineering Düsseldorf") ==
+  #            "Hello Web Engineering Düsseldorf!"
+  # end
+end
